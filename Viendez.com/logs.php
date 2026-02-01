7d55eb6466e065ecdd9cf4be4594c3facf988c0b
franckw31 <wenger.franck@gmail.com>
2026-02-01 15:01:35 +0100

    logs

--- Viendez.com/logs.php
+++ Viendez.com/logs.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/include/functions_logs.php';
 
 
+
+
 // Fonction pour géolocaliser une IP
 function getCity($ip) {
     // Vérifier si c'est une IP locale
